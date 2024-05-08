9f8a6b3197da1801eb75887bc749eb8a5978bde1
divsansi <122143617+divsansi@users.noreply.github.com>
2024-05-08 10:20:53 +0530

    Imported Firebase and Quotes Libraries

---
+++ counselor/src/lib/firebase.ts
@@ -0,0 +1,11 @@
+import { initializeApp } from 'firebase/app';
+
+export const app = initializeApp({
+	apiKey: 'AIzaSyDOPwqqMYwDeskIX-tJEn9JavBNonFnJYA',
+	authDomain: 'finalproject-28368.firebaseapp.com',
+	projectId: 'finalproject-28368',
+	storageBucket: 'finalproject-28368.appspot.com',
+	messagingSenderId: '269091379492',
+	appId: '1:269091379492:web:975fc047207f227511fbb1',
+	measurementId: 'G-CPTZ9B1BEK'
+});
