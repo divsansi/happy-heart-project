7d2fedbb04ec722bf601337fe9a3b55e2e14df41
divsansi <122143617+divsansi@users.noreply.github.com>
2024-05-08 20:49:04 +0530

    Modified the styles of game pages

--- user/src/routes/games/+page.svelte
+++ user/src/routes/games/+page.svelte
@@ -1,34 +1,74 @@
 <script lang="ts">
-	import { SignedIn, User } from 'sveltefire';
+	import { createAvatar } from '@dicebear/core';
+	import { identicon } from '@dicebear/collection';
+	import { SignedIn } from 'sveltefire';
+
+	const avatar = (uid: string) => {
+		const av = createAvatar(identicon, { seed: uid, backgroundType: ['solid'] });
+		return av.toDataUriSync();
+	};
 </script>
 
 <SignedIn let:user>
-	<div
-		class="mx-auto flex max-w-xl flex-col items-center justify-center gap-4 overflow-y-scroll pb-24"
-	>
-		<!-- Welcome Card -->
+	<section id="topBar" class="flex flex-row" style="width: 100%; align-items: center; justify-content: space-between; padding: 25px; margin-bottom: -20px; background: transparent; position: sticky; top: 0; backdrop-filter: blur(25px);">
+		<img class="topbarLogo" src="https://i.postimg.cc/sgx3L1gg/logotext-2.png" alt="Logo">
 
-		<div class="flex w-full flex-col items-center gap-4 p-4">
-			<div class="flex w-full flex-col items-center gap-2">
-				<h6 class="text-xl font-bold font-medium text-gray-900 dark:text-white">Games</h6>
-				<div class="flex w-full flex-col items-center gap-2">
-
-					<div class="flex w-full flex-col gap-4 rounded-lg border p-4 shadow-sm backdrop-blur-md">
-						<span style="height: 70vh; width: 100%; background: var(--whiteGrey); border-radius: 10px;">
-							<div style="display: flex; flex-direction: column; height: 100%; width: 100%; gap: 10px; border: 1.5px solid var(--accent); border-radius: 10px;">
-								<a href="games/memorygame">Memory Game</a>
-								<a href="games/thecube">The Cube</a>
-								<a href="games/snakesandladders">Snakes and Ladders</a>
-								<a href="games/towerblocks">Tower Blocks</a>
-								<a href="games/rockpaperscissors">Rock Paper Scissors</a>
-							</div>
-
-							
-						</span>
-					</div>
-
-				</div>
-			</div>
+		<div class="userpfp">
+			<img src="{avatar(user.uid)}" alt="Profile">
 		</div>
-	</div>
+
+		<script>
+			const userpfp = document.querySelector('.userpfp');
+
+			if (window.location.pathname === '/login' || window.location.pathname === '/register' ||  window.location.pathname === '/logout') {
+				userpfp.style.display = 'none';
+			}
+
+			window.addEventListener('scroll', () => {
+				const topBar = document.getElementById('topBar');
+				if (window.scrollY > 0) {
+					topBar.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
+					topBar.style.transition = 'box-shadow 0.5s';
+				} else {
+					topBar.style.boxShadow = 'none';
+					topBar.style.transition = 'box-shadow 0.5s';
+				}
+			});
+		</script>
+	</section>
+
+	<main>
+
+		<section class="flex flex-col" style="justify-content: center; gap: 20px;">
+			<h1 style="color: white;">Games</h1>
+
+			<span class="flex flex-col" style="gap: 10px;">
+				<a class="flex flex-row" style="background: var(--accent2); padding: 20px 25px; border-radius: 15px; align-items: center; justify-content: space-between;" href="/games/memorygame">
+					<h1 class="light" style="font-size: 14px;">Memory Game</h1>
+					<i class="fa-solid fa-chevron-right" style="color: var(--accent); font-size: 10px;"></i>
+				</a>
+
+				<a class="flex flex-row" style="background: var(--accent2); padding: 20px 25px; border-radius: 15px; align-items: center; justify-content: space-between;" href="/games/rockpaperscissors">
+					<h1 class="light" style="font-size: 14px;">Rock Paper Scissors</h1>
+					<i class="fa-solid fa-chevron-right" style="color: var(--accent); font-size: 10px;"></i>
+				</a>
+
+				<a class="flex flex-row" style="background: var(--accent2); padding: 20px 25px; border-radius: 15px; align-items: center; justify-content: space-between;" href="/games/snakesandladders">
+					<h1 class="light" style="font-size: 14px;">Snakes and Ladders</h1>
+					<i class="fa-solid fa-chevron-right" style="color: var(--accent); font-size: 10px;"></i>
+				</a>
+
+				<a class="flex flex-row" style="background: var(--accent2); padding: 20px 25px; border-radius: 15px; align-items: center; justify-content: space-between;" href="/games/thecube">
+					<h1 class="light" style="font-size: 14px;">The Cube</h1>
+					<i class="fa-solid fa-chevron-right" style="color: var(--accent); font-size: 10px;"></i>
+				</a>
+
+				<a class="flex flex-row" style="background: var(--accent2); padding: 20px 25px; border-radius: 15px; align-items: center; justify-content: space-between;" href="/games/towerblocks">
+					<h1 class="light" style="font-size: 14px;">Tower Blocks</h1>
+					<i class="fa-solid fa-chevron-right" style="color: var(--accent); font-size: 10px;"></i>
+				</a>
+			</span>
+		</section>
+
+	</main>
 </SignedIn>
--- user/src/routes/games/snakesandladders/+page.svelte
+++ user/src/routes/games/snakesandladders/+page.svelte
@@ -1,26 +1,52 @@
 <script lang="ts">
-	import { SignedIn, User } from 'sveltefire';
+	import { createAvatar } from '@dicebear/core';
+	import { identicon } from '@dicebear/collection';
+	import { SignedIn } from 'sveltefire';
+
+	const avatar = (uid: string) => {
+		const av = createAvatar(identicon, { seed: uid, backgroundType: ['solid'] });
+		return av.toDataUriSync();
+	};
 </script>
 
 <SignedIn let:user>
-	<div
-		class="mx-auto flex max-w-xl flex-col items-center justify-center gap-4 overflow-y-scroll pb-24"
-	>
-		<!-- Welcome Card -->
+	<section id="topBar" class="flex flex-row" style="width: 100%; align-items: center; justify-content: space-between; padding: 25px; margin-bottom: -20px; background: transparent; position: sticky; top: 0; backdrop-filter: blur(25px);">
+		<img class="topbarLogo" src="https://i.postimg.cc/sgx3L1gg/logotext-2.png" alt="Logo">
 
-		<div class="flex w-full flex-col items-center gap-4 p-4">
-			<div class="flex w-full flex-col items-center gap-2">
-				<h6 class="text-xl font-bold font-medium text-gray-900 dark:text-white" onclick="window.location.href='/games'">Snakes And Ladders</h6>
-				<div class="flex w-full flex-col items-center gap-2">
-
-					<div class="flex w-full flex-col gap-4 rounded-lg border p-4 shadow-sm backdrop-blur-md">
-						<span style="height: 70vh; width: 100%; background: var(--whiteGrey); border-radius: 10px;">
-							<iframe style="height: 100%; width: 100%; border-radius: 10px;" src="https://happy-heart-games.onrender.com/SnakesAndLadders/index.html" title="Game" frameborder="0"></iframe>
-						</span>
-					</div>
-
-				</div>
-			</div>
+		<div class="userpfp">
+			<img src="{avatar(user.uid)}" alt="Profile">
 		</div>
-	</div>
+
+		<script>
+			const userpfp = document.querySelector('.userpfp');
+
+			if (window.location.pathname === '/login' || window.location.pathname === '/register' ||  window.location.pathname === '/logout') {
+				userpfp.style.display = 'none';
+			}
+
+			window.addEventListener('scroll', () => {
+				const topBar = document.getElementById('topBar');
+				if (window.scrollY > 0) {
+					topBar.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
+					topBar.style.transition = 'box-shadow 0.5s';
+				} else {
+					topBar.style.boxShadow = 'none';
+					topBar.style.transition = 'box-shadow 0.5s';
+				}
+			});
+		</script>
+	</section>
+
+	<main>
+
+		<section class="flex flex-col" style="justify-content: center; gap: 20px;">
+			<a class="flex flex-row" style="color: white; width: fit-content; align-items: center; gap: 5px;" href="/games">
+				<i class="fa-solid fa-chevron-left" style="color: white;"></i>
+				<h1 style="color: white;">Snakes And Ladders</h1>
+			</a>
+
+			<iframe style="height: calc(100vh - 220px); width: 100%; border-radius: 10px;" src="https://happy-heart-games.onrender.com/SnakesAndLadders/index.html" title="Game" frameborder="0"></iframe>
+		</section>
+
+	</main>
 </SignedIn>
