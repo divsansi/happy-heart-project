7a0e334f48a088eaa198d9c943e38a2caad99a5c
divsansi <122143617+divsansi@users.noreply.github.com>
2024-05-09 12:36:32 +0530

    Updated Status page styles

--- user/src/routes/status/+page.svelte
+++ user/src/routes/status/+page.svelte
@@ -7,18 +7,21 @@
 	import { app } from '$lib/firebase';
 	import { addDoc, collection, getFirestore, serverTimestamp } from 'firebase/firestore';
 
-	const avatar = createAvatar(identicon, {
-		seed: 'User',
-		backgroundType: ['gradientLinear', 'solid']
-	});
-
-	const svg = avatar.toDataUriSync();
+	const avatar = (uid: string) => {
+		const av = createAvatar(identicon, { seed: uid, backgroundType: ['solid'] });
+		return av.toDataUriSync();
+	};
 
 	let note = '';
 
 	const db = getFirestore(app);
 
 	const saveNote = (auth: any, user: any) => {
+		// Check if note is empty
+		if (note.trim() === '') {
+			alert('Note cannot be empty');
+			return;
+		}
 		addDoc(collection(db, 'status', user.uid, 'status'), {
 			note: note,
 			createdAt: serverTimestamp(),
@@ -35,49 +38,55 @@
 </script>
 
 <SignedIn let:auth let:user>
-	<!-- Notes -->
-	<Collection ref="status/{user.uid}/status" let:data let:count>
-		<p class="mx-auto max-w-xl p-2 text-sm font-thin text-gray-400 dark:text-white">
-			You have {count} Status Records
-		</p>
-		<div
-			class=" mx-auto flex max-w-xl flex-col items-center justify-center gap-4 overflow-y-scroll pb-36 pl-2 pr-4 pt-4"
-		>
-			{#each orderData(data) as post}
-				{#if post.uid === user.uid}
-					<div class="flex w-full items-start gap-2.5">
-						<!-- svelte-ignore a11y-img-redundant-alt -->
-						<img class="h-8 w-8 rounded-full backdrop-blur-md" src={svg} alt="User image" />
-						<div
-							class="leading-1.5 flex w-full max-w-[320px] flex-col rounded-e-xl rounded-es-xl bg-gray-100 p-2 dark:bg-gray-700"
-						>
-							<div class="flex items-center space-x-2 rtl:space-x-reverse">
-								<!-- <span class="text-sm font-semibold text-gray-900 dark:text-white"
-									>{user.displayName}</span
-								> -->
-								<!-- <span class="text-sm font-normal text-white"
-								>{post.createdAt}</span
-							> -->
-							</div>
-							<p class="py-1 text-sm font-normal text-gray-900 dark:text-white">
-								{post.note}
-							</p>
-						</div>
-					</div>
-				{/if}
-			{/each}
-		</div>
-	</Collection>
+	<section id="topBar" class="flex flex-row" style="width: 100%; align-items: center; justify-content: space-between; padding: 25px; margin-bottom: -20px; background: transparent; position: sticky; top: 0; backdrop-filter: blur(25px);">
+		<img class="topbarLogo" src="https://i.postimg.cc/sgx3L1gg/logotext-2.png" alt="Logo">
 
-	<!-- Add Status Note UI -->
-	<div
-		class="fixed bottom-0 left-0 right-0 mx-auto mb-16 flex max-w-xl flex-row gap-4 p-4 backdrop-blur-md"
-	>
-		<Input type="email" placeholder="Status Note" size="md" bind:value={note} />
-		<Button
-			on:click={() => {
-				saveNote(auth, user);
-			}}>Save</Button
-		>
+		<div class="userpfp">
+			<img src="{avatar(user.uid)}" alt="Profile">
+		</div>
+
+		<script>
+			const userpfp = document.querySelector('.userpfp');
+
+			if (window.location.pathname === '/login' || window.location.pathname === '/register' ||  window.location.pathname === '/logout') {
+				userpfp.style.display = 'none';
+			}
+
+			window.addEventListener('scroll', () => {
+				const topBar = document.getElementById('topBar');
+				if (window.scrollY > 0) {
+					topBar.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
+					topBar.style.transition = 'box-shadow 0.5s';
+				} else {
+					topBar.style.boxShadow = 'none';
+					topBar.style.transition = 'box-shadow 0.5s';
+				}
+			});
+		</script>
+	</section>
+
+	<main>
+
+		<section class="flex flex-col" style="justify-content: center; gap: 20px;">
+			<Collection ref="status/{user.uid}/status" let:data let:count>
+				<h1 style="color: white;">Status ({count} records)</h1>
+
+				<div class="flex flex-col" style="height: calc(100vh - 280px); width: 100%; border-radius: 10px; overflow: auto; gap: 10px;">
+					{#each orderData(data) as post}
+						{#if post.uid === user.uid}
+							<div class="flex flex-row" style="background: var(--accent2); padding: 20px 25px; border-radius: 15px; align-items: center;">
+								<p class="light" style="font-size: 14px; text-transform: none;">{post.note}</p>
+							</div>
+						{/if}
+					{/each}
+				</div>
+			</Collection>
+		</section>
+
+	</main>
+
+	<div class="flex flex-row" style="position: fixed; bottom: 64px; left: 0; height: 60px; width: 100%; background: var(--white); align-items: center; justify-content: center; gap: 10px;">
+		<input type="text" style="height: 35px; width: 70%; font-size: 12px; border-radius: 10px;" placeholder="Status Note" bind:value={note} />
+		<button class="flex flex-row" style="height: 35px; width: 15%; background: #1B72B5; color: #FFFFFF; text-transform: uppercase; font-size: 12px; border: none; border-radius: 10px; align-items: center; justify-content: center; gap: 10px; cursor: pointer;" on:click={() => {saveNote(auth, user);}}>Save</button>
 	</div>
 </SignedIn>
