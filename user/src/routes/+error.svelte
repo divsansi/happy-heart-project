507f93158caa1520404e247cc713059130ad65a4
divsansi <122143617+divsansi@users.noreply.github.com>
2024-05-10 14:11:48 +0530

    Updated styles of error page

--- user/src/routes/+error.svelte
+++ user/src/routes/+error.svelte
@@ -3,19 +3,36 @@
 	import { Button } from 'flowbite-svelte';
 </script>
 
-<div class="flex h-screen w-screen flex-col items-center justify-center">
-	<Section name="page404">
-		<Page404>
-			<svelte:fragment slot="h1">Woops</svelte:fragment>
-			<svelte:fragment slot="paragraph">
-				<p class="mb-4 text-3xl font-bold tracking-tight text-gray-900 md:text-4xl dark:text-white">
-					Something's Went Wrong.
-				</p>
-				<p class="mb-4 text-lg font-light text-white">
-					Sorry, we can't find that page. You'll find lots to explore on the home page.
-				</p>
-				<Button href="/" size="lg" color="red">Back to Homepage</Button>
-			</svelte:fragment>
-		</Page404>
-	</Section>
-</div>
+<section id="topBar" class="flex flex-row" style="width: 100%; align-items: center; justify-content: space-between; padding: 25px; margin-bottom: -20px; background: transparent; position: sticky; top: 0; backdrop-filter: blur(25px);">
+	<img class="topbarLogo" src="https://i.postimg.cc/sgx3L1gg/logotext-2.png" alt="Logo">
+
+	<script>
+		const userpfp = document.querySelector('.userpfp');
+
+		if (window.location.pathname === '/login' || window.location.pathname === '/register' ||  window.location.pathname === '/logout') {
+			userpfp.style.display = 'none';
+		}
+
+		window.addEventListener('scroll', () => {
+			const topBar = document.getElementById('topBar');
+			if (window.scrollY > 0) {
+				topBar.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.1)';
+				topBar.style.transition = 'box-shadow 0.5s';
+			} else {
+				topBar.style.boxShadow = 'none';
+				topBar.style.transition = 'box-shadow 0.5s';
+			}
+		});
+	</script>
+</section>
+
+<main>
+
+	<section class="flex flex-col" style="height: calc(100vh - 220px); width: 100%; align-items: center; justify-content: center; gap: 20px;">
+		<img src="https://i.postimg.cc/tT5TKPmS/404.png" style="height: 250px; margin-bottom: -20px;" alt="404 Logo">
+		<h1 class="medium" style="color: white;">Page Not Found</h1>
+		<p class="light" style="color: white;">Seems like you're lost, but don't worry, it's okay. Maybe try going back to the homepage?</p>
+		<a class="flex flex-row" style="width: 100%; background: #1B72B5; color: #FFFFFF; text-transform: uppercase; padding: 10px; font-size: 12px; border: none; border-radius: 10px; align-items: center; justify-content: center; gap: 10px; cursor: pointer;" href="/">Back to Homepage</a>
+	</section>
+
+</main>
